46f15053c8f7b4a60afbbdd2f47c72d7b9b3a02f
卷毛 <zephyrtang@aliyun.com>
2020-04-06 20:06:13 +0800

    dev : init

--- router/index.js
+++ router/index.js
@@ -1,7 +1,9 @@
 const router = require('koa-router')();
+const render = require('../render').default;
 
+router.get('*', render);
 router.get('/api/hello', (ctx) => {
-    console.log({ ctx });
     ctx.body = 'reply hello!';
+    console.log({ ctx });
 })
 module.exports = router;
