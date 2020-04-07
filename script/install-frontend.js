4a13853876eb38f560fa09e5ca939c5103661502
SunMoon <798063071@qq.com>
2020-04-07 20:22:34 +0800

    添加install方法修改路径以及报错

--- script/install-frontend.js
+++ script/install-frontend.js
@@ -1,17 +1,17 @@
 'use strict';
 
-const path       = require('path');
-const cwd        = process.cwd();
-const fs         = require('fs');
+const path = require('path');
+const cwd = process.cwd();
+const fs = require('fs');
 const folderPath = path.join(cwd, 'web');
-const shell      = require('shelljs');     // 执行shell
-const ora        = require('ora');         // 美化输出
+const shell = require('shelljs');     // 执行shell
+const ora = require('ora');         // 美化输出
 
 
-const CMD_CLONE   = 'git clone git@github.com:zephyr-mo/mini-react-ssr.git web';
+const CMD_CLONE = 'git clone git@github.com:zephyr-mo/mini-react-ssr-web.git web';
 const CMD_INSTALL = 'yarn';
-const CMD_BUILD   = 'yarn build';
-const spinner     = ora();
+const CMD_BUILD = 'yarn build';
+const spinner = ora();
 
 if (fs.existsSync(folderPath)) {
     spinner.start(`[sync] Package Exists . [${folderPath}]`);
@@ -25,6 +25,7 @@ if (fs.existsSync(folderPath)) {
             spinner.fail(`[sync] ERROR: Clone . ${stderr}`);
         } else {
             spinner.succeed(`[sync] DONE: Clone . ${stdout}`);
+            __install();
         }
     })
 }
