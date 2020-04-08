557fd6bd220604d79a2f4f19d9b58ae8461f1f15
卷毛 <zephyrtang@aliyun.com>
2020-04-09 00:03:03 +0800

    dev: 服务端构建客户端脚本

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
 const path = require('path');
-const nodeExternals = require('webpack-node-externals');
-// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// const nodeExternals = require('webpack-node-externals');
+// 构建css前的插件
+const Extract = require('extract-text-webpack-plugin');
+// 生成html并插入对应js、css
+const Html = require('html-webpack-plugin');
+// 清理dist文件夹
+const { CleanWebpackPlugin: Clean } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: ['@babel/polyfill', './index.js'],
+  entry: {
+    spa: './web/src',
+    ssr: './index.js'
+  },
+  devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'js/[name]_[hash:8].js',
   },
   module: {
     rules: [
@@ -17,15 +26,15 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: [['@babel/preset-react', {modules: false}], ['@babel/preset-env', {modules: false}]],
+            presets: ['@babel/preset-react', '@babel/preset-env'],
           }
         }
       },
       {
-        test: /\.css$/,
-        use: {
-          loader: 'css-loader'
-        }
+        test: /\.(css|scss)$/,
+        loader: Extract.extract({
+          use: ['css-loader', 'sass-loader']
+        })
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
@@ -45,7 +54,6 @@ module.exports = {
           {
             loader: 'url-loader',
             options: {
-              limit: 8192,
               name: 'resource/[name].[ext]'   // 路径
             }
           }
@@ -53,12 +61,17 @@ module.exports = {
       }
     ]
   },
-//   plugins: [
-//     new ExtractTextPlugin("css/[name].css")
-//   ],
-  target: 'node',
-  externals: [nodeExternals()],
+  plugins: [
+    new Clean(),
+    new Html({
+      template: './index.html'
+    }),
+    new Extract({
+      filename: 'css/[name]_[hash:8].css'
+    })
+  ],
   devServer: {
-      port: 6001
+      port: 6001,
+      open: true
   }
 };
